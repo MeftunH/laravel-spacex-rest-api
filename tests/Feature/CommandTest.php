1042c258b0f7241b03784044b82b82a41f4497d2
MeftunH <maftunhashimli@gmail.com>
2021-07-14 12:16:11 +0400

    Artisan Command integration test

---
+++ tests/Feature/CommandTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
+use Tests\TestCase;
+
+class CommandTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_command()
+    {
+        $this->artisan('sync:ToDatabase')->expectsConfirmation('Command Worked')
+            ->assertExitCode(1);
+    }
+}
