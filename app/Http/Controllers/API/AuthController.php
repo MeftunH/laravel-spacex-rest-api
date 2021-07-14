98e009bf393b6ee038e7c75f4f1c0974891f8cf3
MeftunH <maftunhashimli@gmail.com>
2021-07-14 13:36:25 +0400

    Passport login and register routes declared.Login and register functions is done

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $validator = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+
+        if (!auth()->attempt($validator)) {
+            return response()->json(['error' => 'Unauthorised'], 401);
+        } else {
+            $success['token'] = auth()->user()->createToken('authToken')->accessToken;
+            $success['user'] = auth()->user();
+            return response()->json(['success' => $success])->setStatusCode(200);
+        }
+    }
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 401);
+        }
+
+        $input = $request->all();
+        $input['password'] = Hash::make($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('authToken')->accessToken;
+        $success['name'] =  $user->name;
+        return response()->json(['success' => $success])->setStatusCode(200);
+    }
+}
